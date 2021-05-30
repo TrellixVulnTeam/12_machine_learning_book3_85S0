e47407961935f9f0037994617d41e614602f20d5
ruka charo <ruka.and.little.charo1@gmail.com>
2021-05-31 00:30:30 +0900

    Study_day21

--- ch13/practice.py
+++ ch13/practice.py
@@ -1,5 +1,6 @@
 import tensorflow as tf
 import numpy as np
+%precision 3
 
 
 #%% テンソルの作成方法
@@ -106,3 +107,91 @@ B = tf.zeros((3, 3))
 
 C = tf.concat([A, B], axis=1)
 print(C.numpy())
+
+
+# Tensorflow Data API
+#%% 既存のテンソルからDatasetを作成
+a = [1.2, 3.4, 7.5, 4.1, 5.0, 1.0]
+ds = tf.data.Dataset.from_tensor_slices(a)
+print(ds)
+
+# Datasetから繰り返し処理を行う
+for item in ds:
+    print(item)
+
+# バッチ数を指定して繰り返し処理
+ds_batch = ds.batch(3)
+for i, elem in enumerate(ds_batch, 1):
+    print('batch{}:'.format(i), elem.numpy())
+
+
+#%% 2つのテンソルを1つのデータセットにする
+# サンプルの準備
+tf.random.set_seed(1)
+
+t_x = tf.random.uniform([4, 3], dtype=tf.float32)
+t_y = tf.range(4)
+
+# データセットの結合(その1)
+ds_x = tf.data.Dataset.from_tensor_slices(t_x)
+ds_y = tf.data.Dataset.from_tensor_slices(t_y)
+
+ds_joint = tf.data.Dataset.zip((ds_x, ds_y))
+
+for example in ds_joint:
+    print('x:', example[0].numpy(),
+            'y:', example[1].numpy())
+
+# データセットの結合(その2)
+ds_joint = tf.data.Dataset.from_tensor_slices((t_x, t_y))
+
+for example in ds_joint:
+    print('x:', example[0].numpy(),
+            'y:', example[1].numpy())
+
+#%% 各データセットに対する処理
+ds_trans = ds_joint.map(lambda x, y: (x*2 - 1.0, y))
+
+for example in ds_trans:
+    print('x:', example[0].numpy(),
+            'y:', example[1].numpy())
+
+
+#%% シャッフル、バッチ、リピート
+tf.random.set_seed(1)
+ds = ds_joint.shuffle(buffer_size=len(t_x))
+for example in ds:
+    print('x: ', example[0].numpy(),
+            'y: ', example[1].numpy())
+
+# バッチで取り出す
+ds = ds_joint.batch(batch_size=3, drop_remainder=False)
+batch_x, batch_y = next(iter(ds))
+print('Batch-x:\n', batch_x.numpy())
+print('Batch-y:', batch_y.numpy())
+
+# リピートで繰り返す
+ds = ds_joint.batch(3).repeat(count=2)
+for i, (batch_x, batch_y) in enumerate(ds):
+    print(i, batch_x.shape, batch_y.numpy())
+
+# リピート → バッチだと結果が変わる
+ds = ds_joint.repeat(count=2).batch(3)
+for i, (batch_x, batch_y) in enumerate(ds):
+    print(i, batch_x.shape, batch_y.numpy())
+
+# バッチ、リピート、シャッフルを色々な順番で
+# シャッフル → バッチ → リピート
+ds = ds_joint.shuffle(len(t_x)).batch(2).repeat(3)
+for i, (batch_x, batch_y) in enumerate(ds):
+    print(i, batch_x.shape, batch_y.numpy())
+
+# バッチ → シャッフル → リピート
+ds = ds_joint.batch(2).shuffle(len(t_x)).repeat(3)
+for i, (batch_x, batch_y) in enumerate(ds):
+    print(i, batch_x.shape, batch_y.numpy())
+
+# バッチ → リピート → シャッフル
+ds = ds_joint.batch(2).repeat(3).shuffle(len(t_x))
+for i, (batch_x, batch_y) in enumerate(ds):
+    print(i, batch_x.shape, batch_y.numpy())
